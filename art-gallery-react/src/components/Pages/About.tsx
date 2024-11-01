9faf6d77b143d2dd19a0be5ec11c3c27dbfcfe4c
Joanne <jungyoon.lim99@gmail.com>
2024-11-02 00:48:38 +0900

    about me page

--- art-gallery-react/src/components/Pages/About.tsx
+++ art-gallery-react/src/components/Pages/About.tsx
@@ -1,52 +1,30 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const BioContainer = styled.div`
-  position: absolute;
-  width: 810px;
-  max-width: calc(100% - 40px);
-  left: 475px;
-  top: 10px;
-  padding: 20px;
-  box-sizing: border-box;
-
-  @media (max-width: 1285px) { 
-    left: 0;
-    width: 100%;
-  }
-
-  @media (max-width: 810px) {
-    position: relative;
-    top: 44px;
-    padding: 20px 0;
-  }
-`;
-
-const Bio = styled.p`
-  font-family: 'Fira Sans', sans-serif;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 36px;
-  color: #333;
-  margin: 0;
-
-  @media (max-width: 810px) {
-    font-size: 24px;
-    line-height: 30px;
-  }
-`;
-
 
 const About: React.FC = () => {
     return (
-            <BioContainer>
-                    <Bio>
-                        <p>Jungyoon Lim (b. 1999, Seoul, Korea) is a design engineer. </p>
-                        <p>She spent her childhood drawing and painting, <br></br> her teenage years reading classics, <br></br>and her early 20s studying math and philosophy, <br></br> but now she designs and codes digital objects. 
-                        </p>
-                    </Bio>
-        </BioContainer>
+        <div className="flex flex-col pt-16 pl-16 
+            lg:pt-12 lg:pl-12
+            md:pt-8 md:pl-8
+            sm:pt-6 sm:pl-6">
+            <div className="max-w-3xl">
+                <p className="font-libre text-[24px] leading-[30px] text-black
+                    lg:text-[22px] lg:leading-[28px]
+                    md:text-[20px] md:leading-[26px]
+                    sm:text-[18px] sm:leading-[24px]">
+                    Jungyoon Lim (b. 1999, Seoul, Korea) is a design engineer. 
+                    
+                    <div className="mt-12 
+                        lg:mt-10 
+                        md:mt-8 
+                        sm:mt-6">
+                        She spent her childhood drawing and painting, <br />
+                        her teenage years reading classics, <br />
+                        and her early 20s studying math and philosophy, <br />
+                        but now she designs and codes digital objects.
+                    </div>
+                </p>
+            </div>
+        </div>
     );
 };
 
