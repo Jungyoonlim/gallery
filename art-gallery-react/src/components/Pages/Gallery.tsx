9f833e7b5fec7008773c9e8a674918fa582ccf30
Joanne <jungyoon.lim99@gmail.com>
2024-12-01 19:37:34 +0900

    work

--- art-gallery-react/src/components/Pages/Gallery.tsx
+++ art-gallery-react/src/components/Pages/Gallery.tsx
@@ -96,7 +96,8 @@ const Gallery: React.FC = () => {
     { id: 9, top: 660, left: 1087, width: 350, height: 277.33, url: "/images/horizontalface.jpg" },
     { id: 10, top: 660, left: 1087, width: 350, height: 277.33, url: "/images/flower.jpg" },
     { id: 11, top: 660, left: 1087, width: 350, height: 277.33, url: "/images/snow-school.jpg"},
-    { id: 12, top: 340, left: 327, width: 350, height: 283.33, url: "/images/met.jpg" }
+    { id: 12, top: 340, left: 327, width: 350, height: 283.33, url: "/images/met.jpg" },
+    { id: 13, top: 660, left: 327, width: 350, height: 263.46, url: "/images/portrait.jpg" },
   ];
 
   return (
